205f54899c34bf0650a96f19b5ed9c85f5dd3d17
alijoghatee <117152318+alijoghatee@users.noreply.github.com>
2022-11-13 10:03:24 +0330

    add test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SignUpTest(TestCase):
+    username = 'testusername'
+    email = 'testusername@gmail.com'
+
+    def test_sign_up_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_sign_up_url(self):
+        response = self.client.get('/account/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_sign_up_form(self):
+        user_model = get_user_model()
+        user = user_model.objects.create_user(
+            self.username,
+            self.email,
+        )
+        self.assertEqual(user_model.objects.all().count(), 1)
+        self.assertEqual(user_model.objects.all()[0].username, self.username)
+        self.assertEqual(user_model.objects.all()[0].email, self.email)
+
+    def test_csrf_token(self):
+        Client(enforce_csrf_checks=True)
