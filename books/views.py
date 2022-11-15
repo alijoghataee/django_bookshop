4d47f72a62afbd20dfc6fb0a0d898e7caf7664a4
alijoghatee <117152318+alijoghatee@users.noreply.github.com>
2022-11-15 18:20:44 +0330

    add detail view

--- books/views.py
+++ books/views.py
@@ -8,3 +8,8 @@ class BookListView(generic.ListView):
     model = Book
     template_name = 'books/books_list_view.html'
     context_object_name = 'books'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/books_detail_view.html'
